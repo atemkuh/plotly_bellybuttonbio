fee38cbbe0df3906282ad5d60de9edd39c2f23d3
joseph <>
2021-01-03 19:49:29 -0500

    slice data and reverse data values

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,20 +1,32 @@
-function buildMetadata(sample) {
+function init() {
+// read data from "sample.json" file
   d3.json("samples.json").then((data) => {
-    var metadata= data.metadata;
-    var resultsarray= metadata.filter(sampleobject => sampleobject.id == sample);
-    var result= resultsarray[0]
-    var PANEL = d3.select("#sample-metadata");
-    PANEL.html("");
-    Object.entries(result).forEach(([key, value]) => {
-      PANEL.append("h6").text(`${key}: ${value}`);
-    });
-
+    var idArr = [];
+    idArr = data.names;
+    var idList = d3.select(selDataset);
+    for (var i = 0; i < idArr.length; i++) {
+      idList.append("option").property("value", i).text(idArr[i]);
+    }
     
-  
-  });
-}
+    var labels = data.samples[0].otu_ids;
+    var values = data.samples[0].sample_values;
+    var hovertext = data.samples[0].otu_labels;
+
+    // create data slices and reverse values
+    var barLabels = labels.slice(0, 10).reverse();
+    var barValues = values.slice(0, 10).reverse();
+    var barHovertext = hovertext.slice(0, 10).reverse();
 
 
 
-// Initialize the dashboard
+
+
+
+
+
+
+
+
+
+
 init();
