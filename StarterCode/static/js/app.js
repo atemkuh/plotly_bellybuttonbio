4622f4025fc1a3d9edb1b3c53b3fa2700d4d8fcb
joseph <>
2021-01-03 19:18:56 -0500

    initial  commit

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,20 @@
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata= data.metadata;
+    var resultsarray= metadata.filter(sampleobject => sampleobject.id == sample);
+    var result= resultsarray[0]
+    var PANEL = d3.select("#sample-metadata");
+    PANEL.html("");
+    Object.entries(result).forEach(([key, value]) => {
+      PANEL.append("h6").text(`${key}: ${value}`);
+    });
+
+    
+  
+  });
+}
+
+
+
+// Initialize the dashboard
+init();
